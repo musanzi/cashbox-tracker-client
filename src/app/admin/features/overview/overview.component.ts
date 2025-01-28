15e5956491e168d31fe86ec13dfc468462e6bcdf
Wilfried Musanzi <musanziw.m@gmail.com>
2025-01-28 14:22:05 +0200

    feat: enhance overview component to display total cashbox balance and update service references

--- src/app/admin/features/overview/overview.component.ts
+++ src/app/admin/features/overview/overview.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { DashboardService } from '../../data-access/admin.service';
+import { OverviewService } from '../../data-access/overview.service';
 import { Observable } from 'rxjs';
 import { IAPIResponse } from '../../../shared/services/api/types/api-response.type';
 import { ICashbox } from '../../../shared/utils/types/models.type';
@@ -7,15 +7,19 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overview',
-  providers: [DashboardService],
+  providers: [OverviewService],
   imports: [CommonModule],
   templateUrl: './overview.component.html'
 })
 export class OverviewComponent implements OnInit {
-  #dashboardService = inject(DashboardService);
-  cashboxes: Observable<IAPIResponse<ICashbox[]>>;
+  #dashboardService = inject(OverviewService);
+  cashboxes$: Observable<IAPIResponse<ICashbox[]>>;
 
   ngOnInit(): void {
-    this.cashboxes = this.#dashboardService.getCashboxes();
+    this.cashboxes$ = this.#dashboardService.getCashboxes();
+  }
+
+  getTotal(cashboxes: ICashbox[]): number {
+    return Math.round(cashboxes.reduce((total, cashbox) => total + Number(cashbox.balance), 0));
   }
 }
