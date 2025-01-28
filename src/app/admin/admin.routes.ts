e73c5ee935f59369161b38eb119071b3e4021841
Wilfried Musanzi <musanziw.m@gmail.com>
2025-01-28 13:48:11 +0200

    feat: update admin routes to include users and transactions; load main component dynamically

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,16 +1,19 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './features/main/main.component';
 import { cashboxesRoutes } from './features/cashboxes/cashboxes.routes';
 import { overviewRoutes } from './features/overview/overview.routes';
+import { usersRoutes } from './features/users/users.routes';
+import { transactionsRoutes } from './features/transactions/transactions.routes';
 
 export const adminRoutes: Routes = [
   {
     path: '',
     title: 'Dashboard',
-    component: MainComponent,
+    loadComponent: () => import('./features/main/main.component').then((c) => c.MainComponent),
     children: [
       { path: '', loadChildren: () => overviewRoutes },
-      { path: 'cashboxes', loadChildren: () => cashboxesRoutes }
+      { path: 'users', loadChildren: () => usersRoutes },
+      { path: 'cashboxes', loadChildren: () => cashboxesRoutes },
+      { path: 'transactions', loadChildren: () => transactionsRoutes }
     ]
   }
 ];
