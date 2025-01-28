3baec383b6e35eb26123a5a814444369d06ad7b9
Wilfried Musanzi <musanziw.m@gmail.com>
2025-01-28 11:44:23 +0200

    feat: bind user data to sidebar component in main layout; update component to use NgRx store

--- src/app/admin/features/main/main.component.ts
+++ src/app/admin/features/main/main.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../ui/sidebar/sidebar.component';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IUser } from '../../../shared/utils/types/models.type';
+import { selectUser } from '../../../shared/store/auth/auth.reducers';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './main.component.html'
 })
-export class MainComponent {}
+export class MainComponent {
+  #store = inject(Store);
+  user$: Observable<IUser>;
+
+  constructor() {
+    this.user$ = this.#store.pipe(select(selectUser));
+  }
+}
--- src/app/admin/ui/sidebar/sidebar.component.ts
+++ src/app/admin/ui/sidebar/sidebar.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { adminLinks } from '../../utils/data/links';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { IUser } from '../../../shared/utils/types/models.type';
+import { ApiImgPipe } from '../../../shared/pipes/api-img.pipe';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, MatIconModule, RouterModule],
+  imports: [CommonModule, MatIconModule, RouterModule, ApiImgPipe],
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
+  user = input.required<IUser>();
   adminLinks = adminLinks;
 }
