f5bf2363ef53fea7d6b008fdd7a243813c03d974
Wilfried Musanzi <musanziw.m@gmail.com>
2025-01-28 13:49:57 +0200

    feat: implement cashboxes service and component; fetch and display cashboxes data

--- src/app/admin/features/cashboxes/list/cashboxes.component.ts
+++ src/app/admin/features/cashboxes/list/cashboxes.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { CashboxesService } from '../../../data-access/cashboxes.service';
+import { Observable } from 'rxjs';
+import { IAPIResponse } from '../../../../shared/services/api/types/api-response.type';
+import { ICashbox } from '../../../../shared/utils/types/models.type';
 
 @Component({
   selector: 'app-cashboxes',
-  imports: [],
+  providers: [CashboxesService],
+  imports: [CommonModule],
   templateUrl: './cashboxes.component.html'
 })
-export class CashboxesComponent {}
+export class CashboxesComponent implements OnInit {
+  #cashboxesService = inject(CashboxesService);
+  cashboxes$: Observable<IAPIResponse<ICashbox[]>>;
+
+  ngOnInit(): void {
+    this.cashboxes$ = this.#cashboxesService.getCashboxes();
+  }
+}
